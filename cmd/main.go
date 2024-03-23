3fd025627e7c1fa1b84959c761d28aee505340c3
David Senack <david.senack@gmail.com>
2024-03-22 21:45:45 -0400

    add main function and test cpu functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,2 +1,17 @@
 // Package cmd contains the main entry point for the Gofetch CLI.
 package cmd
+
+import (
+	"fmt"
+
+	"github.com/davidsenack/gofetch/pkg/cpu"
+)
+
+func main() {
+	model, err := cpu.GetCPUModel()
+	if err != nil {
+		fmt.Println("Error fetching CPU model:", err)
+		return
+	}
+	fmt.Println("CPU Model:", model)
+}
