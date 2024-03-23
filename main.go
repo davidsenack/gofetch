c676fe6a98d37f653acbde002173136b54679591
David Senack <david.senack@gmail.com>
2024-03-22 22:14:20 -0400

    test distro function

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 // Package cmd contains the main entry point for the Gofetch CLI.
-package cmd
+package main
 
 import (
 	"fmt"
 
 	"github.com/davidsenack/gofetch/pkg/cpu"
+	"github.com/davidsenack/gofetch/pkg/distro"
 )
 
 func main() {
@@ -14,4 +15,18 @@ func main() {
 		return
 	}
 	fmt.Println("CPU Model:", model)
+
+	kernel, err := distro.GetKernelVersion()
+	if err != nil {
+		fmt.Println("Error fetching kernel version:", err)
+		return
+	}
+	fmt.Println("Kernel Version:", kernel)
+
+	distro, err := distro.GetDistro()
+	if err != nil {
+		fmt.Println("Error fetching distro:", err)
+		return
+	}
+	fmt.Println("Distro:", distro)
 }
